c6f4f780e4c092309b9500c03774a18736eeb48f
Junyeong Park <bulruboa@gmail.com>
2025-05-20 01:43:13 +0900

    test monster
    Just test and share monster prefab

---
+++ Assets/Mimic/Scripts/Movement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MimicSpace
+{
+    /// <summary>
+    /// This is a very basic movement script, if you want to replace it
+    /// Just don't forget to update the Mimic's velocity vector with a Vector3(x, 0, z)
+    /// </summary>
+    public class Movement : MonoBehaviour
+    {
+        [Header("Controls")]
+        [Tooltip("Body Height from ground")]
+        [Range(0.5f, 5f)]
+        public float height = 0.8f;
+        public float speed = 5f;
+        Vector3 velocity = Vector3.zero;
+        public float velocityLerpCoef = 4f;
+        Mimic myMimic;
+
+        private void Start()
+        {
+            myMimic = GetComponent<Mimic>();
+        }
+
+        void Update()
+        {
+            Vector3 playerPos =  Camera.main.transform.position;
+            Vector3 toPlayer = (playerPos - transform.position);
+            float playerDis = toPlayer.magnitude;
+            toPlayer = toPlayer.normalized;
+
+            Vector3 baseMoveDir = playerDis < 6.3f ? -toPlayer : toPlayer;
+            velocity = baseMoveDir * 4f;
+
+            // Assigning velocity to the mimic to assure great leg placement
+            myMimic.velocity = velocity;
+
+            transform.position += velocity * Time.deltaTime;
+
+            RaycastHit hit;
+            Vector3 destHeight = transform.position;
+            if (Physics.Raycast(transform.position, Vector3.down, out hit))
+                destHeight = new Vector3(transform.position.x, hit.point.y + height, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, destHeight, velocityLerpCoef * Time.deltaTime);
+        }
+    }
+
+}
